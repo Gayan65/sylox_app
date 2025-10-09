1b0a32349837e9a2d3026c02336e343d05528894
Gayan65 <gayan65728@yahoo.com>
2025-10-09 20:19:01 +0300

    nav bar still bug

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,43 @@
 import React from "react";
+import navLogo from "../assets/images/logo/TempLogo.png";
 
 const NavBar = () => {
     return (
-        <nav className="navbar bg-body-tertiary">
-            <div className="container-fluid">
-                <span className="navbar-brand mb-0 h1">Navbar</span>
+        <nav className="navbar navbar-expand-lg ">
+            <div className="container">
+                <a className="navbar-brand" href="/">
+                    <img src={navLogo} alt="navigation logo" width={170} />
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarNavAltMarkup"
+                >
+                    <div className="navbar-nav">
+                        <a className="nav-link" aria-current="page" href="#">
+                            Home
+                        </a>
+                        <a className="nav-link" href="#">
+                            About
+                        </a>
+                        <a className="nav-link" href="#">
+                            Contact
+                        </a>
+                        <a className="nav-link" href="#">
+                            Management
+                        </a>
+                    </div>
+                </div>
             </div>
         </nav>
     );
