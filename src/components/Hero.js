36b45132c3117ccca5038fdf108d681d76640a02
Gayan65 <gayan65728@yahoo.com>
2025-11-10 08:55:02 +0200

    ph hero add

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import slide1 from "../assets/images/hero/1.jpg";
-import slide2 from "../assets/images/hero/2.jpg";
-import slide3 from "../assets/images/hero/3.jpg";
+import slide2 from "../assets/images/hero/2.jpeg";
+import slide3 from "../assets/images/hero/3.jpeg";
 import slide4 from "../assets/images/hero/4.jpg";
-import slide5 from "../assets/images/hero/5.jpg";
+import slide5 from "../assets/images/hero/5.jpeg";
 import slide6 from "../assets/images/hero/6.jpeg";
 import slide7 from "../assets/images/hero/7.jpeg";
 
